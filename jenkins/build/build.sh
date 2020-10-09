8eb64dae226b5c988246c9c08a6017ab21d4a873
ManuelHTP <53838258+ManuelHTP@users.noreply.github.com>
2020-10-09 20:42:35 -0300

    Update build.sh

--- jenkins/build/build.sh
+++ jenkins/build/build.sh
@@ -4,5 +4,6 @@ cp -f java-app/target/*.jar jenkins/build/
 echo "######################"
 echo "*** Building image ***"
 echo "######################"
-cd jenkins/build/ && docker-compose -f docker-compose-build.yml build --no-cache
+cd jenkins/build/
+docker-compose -f docker-compose-build.yml build --no-cache
 echo "FIN"
